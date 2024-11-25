9462c1b0261394407de9c7004c9b4f3635b4e993
Saravanan-vc <saravanan1462004@gmail.com>
2024-11-25 13:46:59 +0530

    splash animation

--- lib/view/login_onboarding_splash/splash_view.dart
+++ lib/view/login_onboarding_splash/splash_view.dart
@@ -1,5 +1,6 @@
 import 'dart:async';
 
+import 'package:commerce/animatioins/transation_animations.dart';
 import 'package:commerce/ui/images_ui.dart';
 // import 'package:commerce/view/amazon_view.dart';
 import 'package:commerce/view/home_view.dart';
@@ -17,42 +18,13 @@ class SplashView extends StatefulWidget {
 class _SplashViewState extends State<SplashView> {
   @override
   void initState() {
-    Timer(const Duration(seconds: 2), () {
+    Timer(const Duration(seconds: 3), () {
       Navigator.of(context).pushReplacement(
-          MaterialPageRoute(builder: (context) => const SplashView1()));
-    });
-    super.initState();
-  }
-
-  @override
-  Widget build(BuildContext context) {
-    return Scaffold(
-      body: Center(
-        child: Image.asset(
-          splashImage,
-          width: 121.w,
+        MaterialPageRoute(
+          builder: (context) => const HomeView(),
+          // const SplashView1(),
         ),
-      ),
-    );
-  }
-}
-
-// second splash screen
-class SplashView1 extends StatefulWidget {
-  const SplashView1({super.key});
-
-  @override
-  State<SplashView1> createState() => _SplashView1State();
-}
-
-class _SplashView1State extends State<SplashView1> {
-  @override
-  void initState() {
-    Timer(const Duration(seconds: 4), () {
-      Navigator.of(context).pushReplacement(MaterialPageRoute(
-           builder: (context) => const //OnboardingView()
-              HomeView()
-              ));
+      );
     });
     super.initState();
   }
@@ -62,24 +34,13 @@ class _SplashView1State extends State<SplashView1> {
     return Scaffold(
       body: Stack(
         children: [
-          SizedBox(
-            height: ScreenUtil().screenHeight,
-          ),
           Center(
             child: Image.asset(
               splashImage,
               width: 121.w,
             ),
           ),
-          Positioned(
-            bottom: -MediaQuery.sizeOf(context).height * 0.14,
-            right: -MediaQuery.sizeOf(context).width * 0.3,
-            child: Image.asset(
-              splashImage1,
-              height: 292.h,
-              width: 295.w,
-            ),
-          ),
+          TransationAnimations.TransationAnimations1(context),
         ],
       ),
     );
